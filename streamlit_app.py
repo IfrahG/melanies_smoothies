2d25c843fd00c18c9f7466a2d710dc89883a388e
IfrahG <ifrahghori126@gmail.com>
2024-07-12 19:11:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,13 @@ ingredients_string=''
           ingredients_string+=fruit_chosen+' '
           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
           fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string +"""','""" + name_on_order + """')"""
         
 
 #st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string +"""','""" + name_on_order + """')"""
+
 
 #st.write(my_insert_stmt)
 #st.stop()
